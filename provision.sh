0fb1dd300f3baae7e066b6de6c470a244c6c81d6
Shay Cohen <shay.cohen@sap.com>
2017-07-23 13:13:31 +0300

    updated

--- provision.sh
+++ provision.sh
@@ -9,33 +9,37 @@ do
 	}
 done
 systemctl restart docker
-DBUSER="root"
-DBPASS="PASS"
 
-
-IMGS=""
-for IMG in $IMGS
+for ZONE in $(firewall-cmd --get-active-zones | egrep -v '^\s') 
 do
-	IMG_FN=$(echo $IMG | sed -e 's/:/IMG_FN_SEMI_COLON/')
-	echo "Verifying / Loading / Pulling image $IMG, this might take a few minutes"
-	[[ -e /vagrant/docker-images/${IMG_FN}.tar ]] && { 
-		sudo docker images | sed -e 's/\s\s*/:/; s/\s.*//' | egrep -q "^$IMG" || sudo docker load -i /vagrant/docker-images/${IMG_FN}.tar || exit 1
-	} || { 
-		sudo docker images | sed -e 's/\s\s*/:/; s/\s.*//' | egrep -q "^$IMG" || sudo docker pull ${IMG}
-		echo "Archiving docker image $IMG, this might take a few minutes"
-		[[ -e /vagrant/docker-images/$(dirname $IMG_FN) ]] || mkdir -p /vagrant/docker-images/$(dirname $IMG_FN)
-		TMPF=$(mktemp)
-		docker save -o $TMPF $IMG
-		mv $TMPF "/vagrant/docker-images/${IMG_FN}.tar"
-	}
+  firewall-cmd --add 25/tcp --zone=$ZONE --permanenet
 done
+firewall-cmd --reload
 
+#-#IMGS=""
+#-#for IMG in $IMGS
+#-#do
+#-#	IMG_FN=$(echo $IMG | sed -e 's/:/IMG_FN_SEMI_COLON/')
+#-#	echo "Verifying / Loading / Pulling image $IMG, this might take a few minutes"
+#-#	[[ -e /vagrant/docker-images/${IMG_FN}.tar ]] && { 
+#-#		sudo docker images | sed -e 's/\s\s*/:/; s/\s.*//' | egrep -q "^$IMG" || sudo docker load -i /vagrant/docker-images/${IMG_FN}.tar || exit 1
+#-#	} || { 
+#-#		sudo docker images | sed -e 's/\s\s*/:/; s/\s.*//' | egrep -q "^$IMG" || sudo docker pull ${IMG}
+#-#		echo "Archiving docker image $IMG, this might take a few minutes"
+#-#		[[ -e /vagrant/docker-images/$(dirname $IMG_FN) ]] || mkdir -p /vagrant/docker-images/$(dirname $IMG_FN)
+#-#		TMPF=$(mktemp)
+#-#		docker save -o $TMPF $IMG
+#-#		mv $TMPF "/vagrant/docker-images/${IMG_FN}.tar"
+#-#	}
+#-#done
+#-#
+#-#
+#-#for CONTAINER in $NAME
+#-#do
+#-#	docker ps -a --format='{{.Names}}' | egrep "^$CONTAINER$" && { 
+#-#		docker rm -f $CONTAINER
+#-#		echo "removing container $CONTAINER"
+#-#	}
+#-#done
 
-for CONTAINER in $NAME
-do
-	docker ps -a --format='{{.Names}}' | egrep "^$CONTAINER$" && { 
-		docker rm -f $CONTAINER
-		echo "removing container $CONTAINER"
-	}
-done
 
